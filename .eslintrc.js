7ee1b7b08cdad6d17867a5700cb846f10d7bec70
thasup <thanachonfirst@hotmail.com>
2022-04-28 00:57:52 +0700

    Setup vue config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,24 @@
 module.exports = {
   root: true,
+
   env: {
     node: true,
   },
+
   extends: [
     'plugin:vue/vue3-essential',
     '@vue/airbnb',
   ],
+
   parserOptions: {
     parser: '@babel/eslint-parser',
   },
+
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-console': 'off',
+    'no-debugger': 'off',
   },
+
   overrides: [
     {
       files: [
@@ -25,4 +30,9 @@ module.exports = {
       },
     },
   ],
+
+  'extends': [
+    'plugin:vue/essential',
+    '@vue/airbnb'
+  ]
 };
