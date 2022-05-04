f886b9d6cc9d91cff1a98c784733ee6adf85fd88
thasup <thanachonfirst@hotmail.com>
2022-05-05 00:48:14 +0700

    Add custom error messages

--- src/components/AppAuth.vue
+++ src/components/AppAuth.vue
@@ -71,7 +71,7 @@
           </form>
           <!-- Registration Form -->
           <vee-form v-show="tab === 'register'" :validation-schema="schema"
-          @submit="register">
+          @submit="register" :initial-values="userData">
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
@@ -101,10 +101,14 @@
             <!-- Password -->
             <div class="mb-3">
               <label class="inline-block mb-2">Password</label>
-              <vee-field type="password" name="password"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                  duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Password" />
+              <vee-field name="password" :bails="false" v-slot="{ field, errors }">
+                <input class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300
+                  transition duration-500 focus:outline-none focus:border-black rounded"
+                  placeholder="Password" type="password" v-bind="field" />
+                <div class="text-red-600" v-for="error in errors" :key="error">
+                  {{ error }}
+                </div>
+              </vee-field>
               <ErrorMessage class="text-red-600" name="password" />
             </div>
             <!-- Confirm Password -->
@@ -131,9 +135,9 @@
             </div>
             <!-- TOS -->
             <div class="mb-3 pl-6">
+              <label class="inline-block">Accept terms of service</label>
               <vee-field type="checkbox" name="tos" value="1"
               class="w-4 h-4 float-left -ml-6 mt-1 rounded" />
-              <label class="inline-block">Accept terms of service</label>
               <ErrorMessage class="text-red-600" name="tos" />
             </div>
             <button type="submit"
@@ -161,9 +165,12 @@ export default {
         email: 'required|min:3|max:100|email',
         age: 'required|minVal:18|maxVal:100',
         password: 'required|min:3|max:100',
-        confirm_password: 'confirmed:@password',
-        country: 'required|excluded:Antartica',
-        tos: 'required',
+        confirm_password: 'password_mismatch:@password',
+        country: 'required|country_excluded:Antartica',
+        tos: 'tos',
+      },
+      userData: {
+        country: 'USA',
       },
     };
   },
