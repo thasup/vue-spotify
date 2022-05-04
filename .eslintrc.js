8aa181a750e23162cd268d447b8aa72c5c3e06fc
thasup <thanachonfirst@hotmail.com>
2022-05-04 22:50:10 +0700

    Validating the form

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vuejs-accessibility/label-has-for': 'warn',
+    'eslint no-tabs': 'off',
   },
 
   overrides: [
