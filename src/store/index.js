65fa2de71fa7ffa18f8a37492f87f27c41a5b903
thasup <thanachonfirst@hotmail.com>
2022-05-12 08:23:36 +0700

    Implement login and signout function

--- src/store/index.js
+++ src/store/index.js
@@ -35,12 +35,22 @@ export default createStore({
 
       commit('toggleAuth');
     },
+    async login({ commit }, payload) {
+      await auth.signInWithEmailAndPassword(payload.email, payload.password);
+
+      commit('toggleAuth');
+    },
     init_login({ commit }) {
       const user = auth.currentUser;
 
       if (user) {
         commit('toggleAuth');
       }
+    },
+    async signout({ commit }) {
+      await auth.signOut();
+
+      commit('toggleAuth');
     },
   },
 });
