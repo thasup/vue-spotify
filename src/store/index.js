3e23542b225ba2d0d76e0cc8f1d1c16bf222c8d0
thasup <thanachonfirst@hotmail.com>
2022-05-30 00:04:51 +0700

    Add format time helper function

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import { createStore } from 'vuex';
 import { auth, usersCollection } from '@/includes/firebase';
 import { Howl } from 'howler';
+import helper from '@/includes/helper';
 
 export default createStore({
   state: {
@@ -26,8 +27,8 @@ export default createStore({
       });
     },
     updatePosition(state) {
-      state.seek = state.sound.seek();
-      state.duration = state.sound.duration();
+      state.seek = helper.formatTime(state.sound.seek());
+      state.duration = helper.formatTime(state.sound.duration());
     },
   },
   getters: {
