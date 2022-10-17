a61cb42d0af8e8b7459ffec12909bbf3ea17e0d2
thasup <thanachonfirst@hotmail.com>
2022-10-17 21:32:57 +0700

    Setting up internalization

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from './store';
 import VeeValidatePlugin from './includes/validation';
 import { auth } from './includes/firebase';
 import Icon from './directives/icon';
+import i18n from './includes/i18n';
 
 import './assets/tailwind.css';
 import './assets/main.css';
@@ -18,6 +19,7 @@ auth.onAuthStateChanged(() => {
     app.use(store);
     app.use(router);
     app.use(VeeValidatePlugin);
+    app.use(i18n);
     app.directive('icon', Icon);
 
     app.mount('#app');
